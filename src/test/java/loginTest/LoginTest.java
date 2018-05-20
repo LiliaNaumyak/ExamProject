8e087bc650e7f064cfc3f65f3378946e2d1b99c0
Lilia <lilia.naumyak@gmail.com>
2018-05-20 23:19:24 +0300

    sleep added

--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -7,7 +7,6 @@ import parentTest.ParentTest;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest extends ParentTest {
-    WebDriver webDriver;
 
     @Test
     public void validLogin (){
@@ -15,7 +14,12 @@ public class LoginTest extends ParentTest {
         homePage.enterLogin("element_5@ukr.net");
         homePage.enterPass("1326547");
         homePage.clickSubmitButton();
-        webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+           e.printStackTrace();
+      }
+
 
         checkAC("Review button is not present", personalAccHomePage.isReviewButtonPresent(), true);
     }
